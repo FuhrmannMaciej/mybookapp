8b7a403750233707c7aa683407f1786fae25afad
fuhmac <belindus@op.pl>
2019-07-27 21:25:21 +0200

    Created repository

---
+++ src/test/java/com/fuhrmannmaciej/mybookapp/MybookappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fuhrmannmaciej.mybookapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MybookappApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
